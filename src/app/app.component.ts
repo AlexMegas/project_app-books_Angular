9c01d5e49155dfc771988bdea824a21c45ebbf9a
Oleksandr Solovei <solovei.oleksandr@gmail.com>
2020-04-14 16:36:30 +0300

    welcome page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-books';
+
+  SignUp() {
+    alert('Pressed button <SignUp>');
+  }
+
+  LogIn() {
+    alert('Pressed button <LogIn>');
+  }
 }
